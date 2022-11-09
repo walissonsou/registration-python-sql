27071cdc7cebb2ae54059c4523e8796a0df29c6e
walissonsou <walissonsouza96@hotmail.com>
2022-11-09 15:08:58 -0300

    Gerando PDF

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from PyQt5 import uic, QtWidgets
+from reportlab.pdfgen import canvas
 import mysql.connector
 
 # criar uma variavel que recebe o mysql.connector 
@@ -53,37 +54,56 @@ def chama_segunda_tela():
     
     ## mostrando os dados do banco
     #método fetchall pega tudo que foi feito no cursor execute, salvo isso na variavel dados_lidos
-    dados_lidos = cursor.fetchall()
-    print(dados_lidos[0][0])
+    dados_lidos = cursor.fetchall()    
     # RouCount - quantas linhas vou ter na tela
     segunda_tela.tableWidget.setRowCount(len(dados_lidos))
      # setcolumnCount - quantas colunas
-    segunda_tela.tableWidget.setColumnCount(4)
+    segunda_tela.tableWidget.setColumnCount(5)
       ## pecorrendo uma matriz
     for i in range(0, len(dados_lidos)):
-      for j in range(0,4):
+      for j in range(0,5):
         segunda_tela.tableWidget.setItem(i,j,QtWidgets.QTableWidgetItem(str(dados_lidos[i][j])))   
     
 def porcentagem():    
-    ## lendo os dados do banco
     cursor = banco.cursor()
     comando_SQL= "SELECT sexo FROM herois;"        
     cursor.execute(comando_SQL)    
     dados_lidos = cursor.fetchall()   
+  
+def gerar_pdf():     
+    cursor = banco.cursor()   
+    comando_SQL = "SELECT * FROM herois"
+    cursor.execute(comando_SQL)
+    dados_lidos = cursor.fetchall()     
+    y = 0
+    pdf = canvas.Canvas("escolhas_superheroi.pdf")
+    pdf.setFont("Arial", 12)
+    pdf.drawString(200,800, "Produtos cadastrados: ")
+    pdf.setFont('Arial', 18)
     
+    pdf.drawString(10,750, "ID")
+    pdf.drawString(100,750, "NAME")
+    pdf.drawString(200,750, "AGE")
+    pdf.drawString(300,750, "TEAM")
+    pdf.drawString(400,750, "SEX")
     
-        
-        
-        
-        
-        
-        
-        
+    for i in range(0, len(dados_lidos)):
+        y = y + 50
+        pdf.drawString(10,750 - y, str(dados_lidos[i][0]))
+        pdf.drawString(100,750 -y, str(dados_lidos[i][1]))
+        pdf.drawString(200,750 -y, str(dados_lidos[i][2]))
+        pdf.drawString(300,750 -y, str(dados_lidos[i][3]))
+        pdf.drawString(400,750 -y, str(dados_lidos[i][4]))
+    
+    pdf.save()
+    print('PDF GERADO COM SUCESSO')
+
 app=QtWidgets.QApplication([])
 formulario=uic.loadUi("formulario.ui")
 segunda_tela=uic.loadUi("listar_datas.ui")
 formulario.pushButton.clicked.connect(funcao_principal)
 formulario.pushButton_2.clicked.connect(chama_segunda_tela)
+segunda_tela.pushButton.clicked.connect(gerar_pdf)
 
 formulario.show()
 app.exec()
