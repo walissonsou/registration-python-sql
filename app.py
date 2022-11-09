c54284c5c1f0705d08723a92766f9294b3f230d2
walissonsou <walissonsouza96@hotmail.com>
2022-11-09 14:35:15 -0300

    Trazendo a porcentagem

--- app.py
+++ app.py
@@ -47,16 +47,38 @@ def chama_segunda_tela():
     segunda_tela.show()
     ## lendo os dados do banco
     cursor = banco.cursor()
+    comando_buscarescolha = "SELECT sexo FROM herois;"
     comando_SQL = "SELECT * FROM herois"
-    cursor.execute(comando_SQL)
+    cursor.execute(comando_SQL)    
     
     ## mostrando os dados do banco
     #método fetchall pega tudo que foi feito no cursor execute, salvo isso na variavel dados_lidos
     dados_lidos = cursor.fetchall()
-    print(dados_lidos)
+    print(dados_lidos[0][0])
+    # RouCount - quantas linhas vou ter na tela
+    segunda_tela.tableWidget.setRowCount(len(dados_lidos))
+     # setcolumnCount - quantas colunas
+    segunda_tela.tableWidget.setColumnCount(4)
+      ## pecorrendo uma matriz
+    for i in range(0, len(dados_lidos)):
+      for j in range(0,4):
+        segunda_tela.tableWidget.setItem(i,j,QtWidgets.QTableWidgetItem(str(dados_lidos[i][j])))   
+    
+def porcentagem():    
+    ## lendo os dados do banco
+    cursor = banco.cursor()
+    comando_SQL= "SELECT sexo FROM herois;"        
+    cursor.execute(comando_SQL)    
+    dados_lidos = cursor.fetchall()   
     
     
-  
+        
+        
+        
+        
+        
+        
+        
 app=QtWidgets.QApplication([])
 formulario=uic.loadUi("formulario.ui")
 segunda_tela=uic.loadUi("listar_datas.ui")
